3412dab66431c0164b2d4ac8658dd4206e8bbd33
Jukka Pallaskivi <jukka.pallaskivi@stud.pointcollege.fi>
2017-03-14 11:58:07 +0200

    Ensimmäinen sovellusversio GitHubiin.

---
+++ XamarinApp1/XamarinApp1/EmployeePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmployeePage : ContentPage
+    {
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            employeeList.ItemsSource = new string[] { "AAA", "BBB" };
+        }
+
+        public async void LoadEmployees(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://pointcol-timesheetmobile.azurewebsites.net");
+                string json = await client.GetStringAsync("/api/employee");
+                string[] employees = JsonConvert.DeserializeObject<string[]>(json);
+
+                employeeList.ItemsSource = employees;
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.GetType().Name + ": " + ex.Message;
+                employeeList.ItemsSource = new string[] { errorMessage };
+            }
+        }
+
+        private void ListWorkAssignments(object sender, EventArgs e)
+        {
+            string employee = employeeList.SelectedItem.ToString();
+            Navigation.PushAsync(new WorkAssignmentPage());
+        }
+    }
+}
---
+++ XamarinApp1/XamarinApp1/WorkAssignmentPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WorkAssignmentPage : ContentPage
+    {
+        public WorkAssignmentPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void LoadWorkAssignments(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://pointcol-timesheetmobile.azurewebsites.net");
+                string json = await client.GetStringAsync("/api/workassignment");
+                string[] assignments = JsonConvert.DeserializeObject<string[]>(json);
+
+                assignmentList.ItemsSource = assignments;
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.GetType().Name + ": " + ex.Message;
+                assignmentList.ItemsSource = new string[] { errorMessage };
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
